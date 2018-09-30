a851b17108c743edabe59bc31fa3514d9dafb3a3
ThousanDIY <tomorrow56@gmail.com>
2018-09-30 16:25:40 +0900

    SPRESENSE+ESP8266+Ambient demo

---
+++ example/SPRESENSE_ESP8266_ambient/Ambient_spr.cpp
@@ -0,0 +1,535 @@
+/*
+ * ambient.cpp - Library for sending data to Ambient
+ * Created by Takehiko Shimojima, April 21, 2016
+ */
+#include "Ambient_spr.h"
+
+#define AMBIENT_DEBUG 1
+
+#if AMBIENT_DEBUG
+#define DBG(...) { Serial.print(__VA_ARGS__); }
+#define ERR(...) { Serial.print(__VA_ARGS__); }
+#else
+#define DBG(...)
+#define ERR(...)
+#endif /* AMBIENT_DBG */
+
+const char* AMBIENT_HOST = "54.65.206.59";
+int AMBIENT_PORT = 80;
+const char* AMBIENT_HOST_DEV = "192.168.0.11";
+int AMBIENT_PORT_DEV = 4567;
+
+const char * ambient_keys[] = {"\"d1\":\"", "\"d2\":\"", "\"d3\":\"", "\"d4\":\"", "\"d5\":\"", "\"d6\":\"", "\"d7\":\"", "\"d8\":\"", "\"lat\":\"", "\"lng\":\"", "\"created\":\""};
+
+Ambient::Ambient() {
+}
+
+bool
+// Ambient::begin(unsigned int channelId, const char * writeKey, WiFiClient * c, int dev) {
+Ambient::begin(unsigned int channelId, const char * writeKey, int dev) {
+  if(AMBIENT_DEBUG){
+    Serial.println("Initialize Ambient");
+  }
+
+   this->channelId = channelId;
+
+    if (sizeof(writeKey) > AMBIENT_WRITEKEY_SIZE) {
+        ERR("writeKey length > AMBIENT_WRITEKEY_SIZE");
+        return false;
+    }
+    strcpy(this->writeKey, writeKey);
+
+//    if(NULL == c) {
+//        ERR("Socket Pointer is NULL, open a socket.");
+//        return false;
+//    }
+//    this->client = c;
+    this->dev = dev;
+    if (dev) {
+        strcpy(this->host, AMBIENT_HOST_DEV);
+        this->port = AMBIENT_PORT_DEV;
+    } else {
+        strcpy(this->host, AMBIENT_HOST);
+        this->port = AMBIENT_PORT;
+    }
+    for (int i = 0; i < AMBIENT_NUM_PARAMS; i++) {
+        this->data[i].set = false;
+    }
+    if(AMBIENT_DEBUG){
+      Serial.println("Host: " + (String)this->host + ", Port: " + this->port);
+      Serial.println("CannelID: " + (String)this->channelId + ", Write Key: " + (String)this->writeKey);
+    }
+
+    return true;
+}
+
+bool
+Ambient::set(int field,const char * data) {
+    --field;
+    if (field < 0 || field >= AMBIENT_NUM_PARAMS) {
+        return false;
+    }
+    if (strlen(data) > AMBIENT_DATA_SIZE) {
+        return false;
+    }
+    this->data[field].set = true;
+    strcpy(this->data[field].item, data);
+
+    Serial.println(String(field) + ", " + String(this->data[field].item));
+
+    return true;
+}
+
+bool Ambient::set(int field, double data)
+{
+	return set(field,String(data).c_str());
+}
+
+bool Ambient::set(int field, int data)
+{
+	return set(field, String(data).c_str());
+}
+
+bool
+Ambient::clear(int field) {
+    --field;
+    if (field < 0 || field >= AMBIENT_NUM_PARAMS) {
+        return false;
+    }
+    this->data[field].set = false;
+
+    return true;
+}
+
+bool
+Ambient::send() {
+
+	uint16_t READBUFFSIZ = 0xFF;
+	char readbuffer[READBUFFSIZ];
+	String cmd;
+	int retry;
+
+	for (retry = 0; retry < AMBIENT_MAX_RETRY; retry++) {
+		int ret;
+//        ret = this->client->connect(this->host, this->port);
+		ret = this->connect(this->host, this->port);
+		if (ret) {
+			break ;
+		}
+	}
+
+  if(retry == AMBIENT_MAX_RETRY) {
+      ERR("Could not connect socket to host\r\n");
+      return false;
+  }
+
+  char str[180];
+  char body[192];
+  char inChar;
+
+  memset(body, 0, sizeof(body));
+  strcat(body, "{\"writeKey\":\"");
+  strcat(body, this->writeKey);
+  strcat(body, "\",");
+
+  for (int i = 0; i < AMBIENT_NUM_PARAMS; i++) {
+    if (this->data[i].set) {
+      strcat(body, ambient_keys[i]);
+      strcat(body, this->data[i].item);
+      strcat(body, "\",");
+    }
+  }
+  body[strlen(body) - 1] = '\0';
+  strcat(body, "}\r\n");
+
+  memset(str, 0, sizeof(str));
+  sprintf(str, "POST /api/v2/channels/%d/data HTTP/1.1\r\n", this->channelId);
+  if (this->port == 80) {
+    sprintf(&str[strlen(str)], "Host: %s\r\n", this->host);
+  } else {
+    sprintf(&str[strlen(str)], "Host: %s:%d\r\n", this->host, this->port);
+  }
+  sprintf(&str[strlen(str)], "Content-Length: %d\r\n", strlen(body));
+  sprintf(&str[strlen(str)], "Content-Type: application/json\r\n\r\n");
+
+  DBG("sending: ");DBG(strlen(str));DBG(" bytes\r\n");DBG(str);
+
+  int ret;
+//    ret = this->client->print(str);
+	ret = this->print(str, "SEND OK");
+
+  delay(30);
+  DBG(ret);DBG(" bytes sent\n\n");
+  if (ret == 0) {
+    ERR("send failed\n");
+    return false;
+  }
+//    ret = this->client->print(body);
+  ret = this->print(body, "SEND OK");
+
+  delay(30);
+  DBG(ret);DBG(" bytes sent\n\n");
+  if (ret == 0) {
+    ERR("send failed\n");
+    return false;
+  }
+
+//    while (this->client->available()) {
+  while (Serial2.available()) {
+//        inChar = this->client->read();
+    inChar = Serial2.read();
+    if(AMBIENT_DEBUG){
+      Serial.write(inChar);
+    }
+  }
+
+//    this->client->stop();
+	this->stop();
+
+  for (int i = 0; i < AMBIENT_NUM_PARAMS; i++) {
+    this->data[i].set = false;
+  }
+  return true;
+}
+
+int
+Ambient::bulk_send(char *buf) {
+
+    int retry;
+    for (retry = 0; retry < AMBIENT_MAX_RETRY; retry++) {
+        int ret;
+//        ret = this->client->connect(this->host, this->port);
+		ret = this->connect(this->host, this->port);
+        if (ret) {
+            break ;
+        }
+    }
+    if(retry == AMBIENT_MAX_RETRY) {
+        ERR("Could not connect socket to host\r\n");
+        return -1;
+    }
+
+    char str[180];
+    char inChar;
+
+    memset(str, 0, sizeof(str));
+    sprintf(str, "POST /api/v2/channels/%d/dataarray HTTP/1.1\r\n", this->channelId);
+    if (this->port == 80) {
+        sprintf(&str[strlen(str)], "Host: %s\r\n", this->host);
+    } else {
+        sprintf(&str[strlen(str)], "Host: %s:%d\r\n", this->host, this->port);
+    }
+    sprintf(&str[strlen(str)], "Content-Length: %d\r\n", strlen(buf));
+    sprintf(&str[strlen(str)], "Content-Type: application/json\r\n\r\n");
+
+    DBG("sending: ");DBG(strlen(str));DBG("bytes\r\n");DBG(str);
+
+    int ret;
+//    ret = this->client->print(str); // send header
+	ret = this->print(str, "SEND OK");
+    delay(30);
+    DBG(ret);DBG(" bytes sent\n\n");
+    if (ret == 0) {
+        ERR("send failed\n");
+        return -1;
+    }
+
+    int sent = 0;
+    unsigned long starttime = millis();
+    while ((millis() - starttime) < AMBIENT_TIMEOUT) {
+//        ret = this->client->print(&buf[sent]);
+		ret = this->print(&buf[sent], "SEND OK");
+        delay(30);
+        DBG(ret);DBG(" bytes sent\n\n");
+        if (ret == 0) {
+            ERR("send failed\n");
+            return -1;
+        }
+        sent += ret;
+        if (sent >= strlen(buf)) {
+            break;
+        }
+    }
+    delay(500);
+
+//    while (this->client->available()) {
+	while (Serial2.available()) {
+//        inChar = this->client->read();
+		inChar = Serial2.read();
+		if(AMBIENT_DEBUG){
+			Serial.write(inChar);
+		}
+	}
+
+//    this->client->stop();
+	this->stop();
+
+    for (int i = 0; i < AMBIENT_NUM_PARAMS; i++) {
+        this->data[i].set = false;
+    }
+
+    return (sent == 0) ? -1 : sent;
+}
+
+bool
+Ambient::delete_data(const char * userKey) {
+    int retry;
+    for (retry = 0; retry < AMBIENT_MAX_RETRY; retry++) {
+        int ret;
+//        ret = this->client->connect(this->host, this->port);
+		ret = this->connect(this->host, this->port);
+        if (ret) {
+            break ;
+        }
+    }
+    if(retry == AMBIENT_MAX_RETRY) {
+        ERR("Could not connect socket to host\r\n");
+        return false;
+    }
+
+    char str[180];
+    char inChar;
+
+    memset(str, 0, sizeof(str));
+    sprintf(str, "DELETE /api/v2/channels/%d/data?userKey=%s HTTP/1.1\r\n", this->channelId, userKey);
+    if (this->port == 80) {
+        sprintf(&str[strlen(str)], "Host: %s\r\n", this->host);
+    } else {
+        sprintf(&str[strlen(str)], "Host: %s:%d\r\n", this->host, this->port);
+    }
+    sprintf(&str[strlen(str)], "Content-Length: 0\r\n");
+    sprintf(&str[strlen(str)], "Content-Type: application/json\r\n\r\n");
+    DBG(str);
+
+    int ret;
+//    ret = this->client->print(str);
+	ret = this->print(str, "SEND OK");
+    delay(30);
+    DBG(ret);DBG(" bytes sent\r\n");
+    if (ret == 0) {
+        ERR("send failed\r\n");
+        return false;
+    }
+
+//    while (this->client->available()) {
+	while (Serial2.available()) {
+//        inChar = this->client->read();
+		inChar = Serial2.read();
+		if(AMBIENT_DEBUG){
+			Serial.write(inChar);
+		}
+	}
+
+//    this->client->stop();
+	this->stop();
+
+    for (int i = 0; i < AMBIENT_NUM_PARAMS; i++) {
+        this->data[i].set = false;
+    }
+
+    return true;
+}
+
+bool
+Ambient::connect(char* host, int port) {
+
+	uint16_t READBUFFSIZ = 0xFF;
+	char readbuffer[READBUFFSIZ];
+
+	String cmd = "AT+CIPSTART=\"TCP\",\"";
+	cmd += host;
+	cmd += "\",";
+	cmd += port;
+
+	// flush read buffer of ESP8266
+	while(Serial2.available()) {
+		Serial2.read();
+		delay(1);
+	}
+
+	Serial2.println(cmd);
+	if(AMBIENT_DEBUG){
+		Serial.println(cmd);
+	}
+
+	uint16_t replyidx = 0;
+	// clear readbuffer
+	memset(readbuffer, 0, sizeof(readbuffer));
+
+	while (true) {
+		while(Serial2.available()) {
+			char c =  Serial2.read();
+			if(AMBIENT_DEBUG){
+				Serial.write(c);
+			}
+			if (c == '\r'){
+				continue;
+			}
+			if (c == 0xA) {
+				if (replyidx == 0){
+					continue;  // the first 0x0A is ignored
+				}
+			}
+			readbuffer[replyidx] = c;
+			// Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
+			replyidx++;
+		}
+		if (strstr(readbuffer, "OK")){
+			if(AMBIENT_DEBUG){
+				Serial.println("*** Connect OK ***");
+			}
+			return true;
+		}
+		delay(500);
+	}
+	return false;
+}
+
+bool
+Ambient::stop() {
+	uint16_t READBUFFSIZ = 0xFF;
+	char readbuffer[READBUFFSIZ];
+
+	String cmd = "AT+CIPCLOSE";
+
+	Serial2.println(cmd);
+	if(AMBIENT_DEBUG){
+		Serial.println(cmd);
+	}
+
+	while(!Serial2.available()){
+		delay(1);
+	}
+
+	uint16_t replyidx = 0;
+
+	// clear readbuffer
+	memset(readbuffer, 0, sizeof(readbuffer));
+
+	while (true) {
+		while(Serial2.available()) {
+			char c =  Serial2.read();
+			if(AMBIENT_DEBUG){
+				Serial.write(c);
+			}
+			if (c == '\r'){
+				continue;
+			}
+			if (c == 0xA) {
+				if (replyidx == 0){
+					// the first 0x0A is ignored
+					continue;
+				}
+			}
+			readbuffer[replyidx] = c;
+			// Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
+			replyidx++;
+		}
+//      Serial.print("<--- "); Serial.println(readbuffer);
+		if (strstr(readbuffer, "OK")){
+			if(AMBIENT_DEBUG){
+				Serial.println("\n*** CLOSED ***");
+			}
+			return true;
+		}
+	}
+	return false;
+}
+
+int
+Ambient::print(char *send, char *reply){
+	uint16_t READBUFFSIZ = 0xFF;
+	char readbuffer[READBUFFSIZ];
+
+	int ret;
+
+ // flush input
+  while(Serial2.available()) {
+    Serial2.read();
+	  delay(1);
+	}
+
+	String cmd = "AT+CIPSEND=";
+	cmd += strlen(send);
+
+	Serial2.println(cmd);
+	if(AMBIENT_DEBUG){
+		Serial.println(cmd);
+	}
+
+	uint16_t replyidx = 0;
+
+	// clear readbuffer
+	memset(readbuffer, 0, sizeof(readbuffer));
+
+	while (true) {
+		while(Serial2.available()) {
+			char c =  Serial2.read();
+			if(AMBIENT_DEBUG){
+				Serial.write(c);
+			}
+			if (c == '\r'){
+				continue;
+			}
+			if (c == 0xA) {
+				if (replyidx == 0){
+					// the first 0x0A is ignored
+					continue;
+				}
+			}
+			readbuffer[replyidx] = c;
+			// Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
+			replyidx++;
+		}
+//		  Serial.print("<--- "); Serial.println(readbuffer);
+		if (strstr(readbuffer, ">")){
+			ret = strlen(send);
+			if(AMBIENT_DEBUG){
+				Serial.println("\n*** Ready to send " + String(ret) + " bytes ***");
+			}
+			break;
+		}
+	}
+
+	Serial2.println(send);
+	if(AMBIENT_DEBUG){
+		Serial.println("*** send data ***");
+		Serial.print(send);
+		Serial.println("*****************");
+	}
+
+	while(!Serial2.available()){
+		delay(1);
+	}
+
+	while (true) {
+		while(Serial2.available()) {
+			char c =  Serial2.read();
+			if(AMBIENT_DEBUG){
+				Serial.write(c);
+			}
+			if (c == '\r'){
+				continue;
+			}
+			if (c == 0xA) {
+				if (replyidx == 0){
+					// the first 0x0A is ignored
+					continue;
+				}
+			}
+			readbuffer[replyidx] = c;
+			// Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
+			replyidx++;
+		}
+//      Serial.print("<--- "); Serial.println(readbuffer);
+		if (strstr(readbuffer, reply)){
+			ret = strlen(send);
+			if(AMBIENT_DEBUG){
+				Serial.println("\n***" + String(ret) + " bytes sent ***");
+			}
+			break;
+		}
+	}
+
+	return ret;
+}
---
+++ example/SPRESENSE_ESP8266_ambient/SPRESENSE_ESP8266_ambient.ino
@@ -0,0 +1,262 @@
+// This is a super simple demo program for ESP8266's that can be used by SPRESENSE
+// Serial2 @ 115200 baud.
+#include "Ambient_spr.h"
+
+#define SSID "SSID"      //your wifi ssid here
+#define PASS "PASSWD"   //your wifi key here
+
+#define LED_PIN LED0
+
+#define REPLYBUFFSIZ 0xFFFF
+char replybuffer[REPLYBUFFSIZ];
+uint8_t getReply(char *send, uint16_t timeout = 500, boolean echo = true);
+uint8_t espreadline(uint16_t timeout = 500, boolean multiline = false);
+boolean sendCheckReply(char *send, char *reply, uint16_t timeout = 500);
+
+enum {WIFI_ERROR_NONE=0, WIFI_ERROR_AT, WIFI_ERROR_RST, WIFI_ERROR_SSIDPWD, WIFI_ERROR_SERVER, WIFI_ERROR_UNKNOWN};
+
+Ambient ambient;
+
+unsigned int channelId = xxxx; // AmbientのチャネルID
+const char* writeKey = "xxxxxxxxxx"; // ライトキー
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+
+  //blink LED0 to indicate power up
+  for(int i = 0; i<3; i++)
+  {
+   digitalWrite(LED_PIN,HIGH);
+   delay(50);
+   digitalWrite(LED_PIN,LOW);
+   delay(100);
+  }
+
+  // Serial debug console
+  Serial.begin(115200);
+
+  //Open software serial for chatting to ESP
+  Serial2.begin(115200);
+
+  Serial.println(F("SPRESENSE ESP8266 Demo"));
+
+  ambient.begin(channelId, writeKey); // チャネルIDとライトキーを指定してAmbientの初期化
+
+  //connect to the wifi
+  byte err = setupWiFi();
+
+  if (err) {
+    // error, print error code
+    Serial.print("setup error:");  Serial.println((int)err);
+
+    debugLoop();
+  }
+
+  // success, print IP
+  getIP();
+
+  //set TCP server timeout
+  //sendCheckReply("AT+CIPSTO=0", "OK");
+}
+
+void loop()
+{
+  // 適当なデータをAmbientに送信する
+  ambient.set(1, "33.50");
+  ambient.set(2, "25.50");
+  ambient.set(3, "95000");
+  ambient.set(4, "30.2");
+
+  ambient.send();
+
+  delay(10000);
+
+}
+
+boolean getVersion() {
+  // Get version?
+  getReply("AT+GMR", 100, true);
+  while (true) {
+    espreadline(50);  // this is the 'echo' from the data
+    Serial.print("<--- "); Serial.println(replybuffer);
+    if (strstr(replybuffer, "OK"))
+    break;
+  }
+  return true;
+}
+
+boolean espReset() {
+  getReply("AT+RST", 1000, true);
+  if (! strstr(replybuffer, "OK")) return false;
+  delay(500);
+
+  // turn off echo
+  getReply("ATE0", 250, true);
+
+  return true;
+}
+
+boolean ESPconnectAP(char *s, char *p) {
+
+  getReply("AT+CWMODE=1", 500, true);
+  if (! (strstr(replybuffer, "OK") || strstr(replybuffer, "no change")) )
+    return false;
+
+  String connectStr = "AT+CWJAP=\"";
+  connectStr += SSID;
+  connectStr += "\",\"";
+  connectStr += PASS;
+  connectStr += "\"";
+  connectStr.toCharArray(replybuffer, REPLYBUFFSIZ);
+  getReply(replybuffer, 200, true);
+
+  while (true) {
+    espreadline(200);  // this is the 'echo' from the data
+    if((String)replybuffer == ""){
+      Serial.print(".");
+    }else{
+      Serial.println("");
+      Serial.print("<--- "); Serial.println(replybuffer);
+    }
+    if (strstr(replybuffer, "OK"))
+    break;
+  }
+
+  return true;
+}
+
+
+byte setupWiFi() {
+  // reset WiFi module
+  Serial.println(F("Soft resetting..."));
+  if (!espReset())
+    return WIFI_ERROR_RST;
+
+  delay(500);
+
+  Serial.println(F("Checking for ESP AT response"));
+  if (!sendCheckReply("AT", "OK"))
+    return WIFI_ERROR_AT;
+
+  getVersion();
+
+  Serial.print(F("Connecting to ")); Serial.println(SSID);
+  if (!ESPconnectAP(SSID, PASS))
+    return WIFI_ERROR_SSIDPWD;
+
+  Serial.println(F("Single Client Mode"));
+
+  if (!sendCheckReply("AT+CIPMUX=0", "OK"))
+        return WIFI_ERROR_SERVER;
+
+  return WIFI_ERROR_NONE;
+}
+
+boolean getIP() {
+  getReply("AT+CIFSR", 100, true);
+  while (true) {
+    espreadline(50);  // this is the 'echo' from the data
+    Serial.print("<--- "); Serial.println(replybuffer);
+    if (strstr(replybuffer, "OK"))
+    break;
+  }
+
+  delay(100);
+
+  return true;
+}
+
+
+
+
+/************************/
+uint8_t espreadline(uint16_t timeout, boolean multiline) {
+  uint16_t replyidx = 0;
+
+  while (timeout--) {
+    if (replyidx > REPLYBUFFSIZ-1) break;
+
+    while(Serial2.available()) {
+      char c =  Serial2.read();
+      if (c == '\r') continue;
+      if (c == 0xA) {
+        if (replyidx == 0)   // the first 0x0A is ignored
+          continue;
+
+        if (!multiline) {
+          timeout = 0;         // the second 0x0A is the end of the line
+          break;
+        }
+      }
+      replybuffer[replyidx] = c;
+      // Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
+      replyidx++;
+    }
+
+    if (timeout == 0) break;
+    delay(1);
+  }
+  replybuffer[replyidx] = 0;  // null term
+  return replyidx;
+}
+
+uint8_t getReply(char *send, uint16_t timeout, boolean echo) {
+  // flush input
+  while(Serial2.available()) {
+     Serial2.read();
+     delay(1);
+  }
+
+  if (echo) {
+    Serial.print("---> "); Serial.println(send);
+  }
+  Serial2.println(send);
+
+  // eat first reply sentence (echo)
+  uint8_t readlen = espreadline(timeout);
+
+  //Serial.print("echo? "); Serial.print(readlen); Serial.print(" vs "); Serial.println(strlen(send));
+
+  if (strncmp(send, replybuffer, readlen) == 0) {
+    // its an echo, read another line!
+    readlen = espreadline();
+  }
+
+  if (echo) {
+    Serial.print ("<--- "); Serial.println(replybuffer);
+  }
+  return readlen;
+}
+
+boolean sendCheckReply(char *send, char *reply, uint16_t timeout) {
+
+  getReply(send, timeout, true);
+
+ /*
+  for (uint8_t i=0; i<strlen(replybuffer); i++) {
+    Serial.print(replybuffer[i], HEX); Serial.print(" ");
+  }
+  Serial.println();
+  for (uint8_t i=0; i<strlen(reply); i++) {
+    Serial.print(reply[i], HEX); Serial.print(" ");
+  }
+  Serial.println();
+*/
+  return (strcmp(replybuffer, reply) == 0);
+}
+
+void debugLoop() {
+  Serial.println("========================");
+  //serial loop mode for diag
+  while(1) {
+    if (Serial.available()) {
+      Serial2.write(Serial.read());
+//      delay(1);
+    }
+    if (Serial2.available()) {
+      Serial.write(Serial2.read());
+//      delay(1);
+    }
+  }
+}
